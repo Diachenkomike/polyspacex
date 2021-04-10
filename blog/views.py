c0130e21d05ccd0094a6c3004f90674bc1b65b14
django_girl <diachenkomike9@gmail.com>
2021-04-10 23:09:58 +0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from django.utils import timezone
 # Create your views here.
 def post_list(request):
+	
     return render(request, 'blog/post_list.html', {})
